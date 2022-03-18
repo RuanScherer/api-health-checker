5c0c3cfdb0c6612d8f369063474266f32335d44a
Vinicius Geisler <vinicius.geisler138@gmail.com>
2022-03-18 13:02:18 -0300

    feat: Backend Creation

---
+++ backend/src/routes/index.ts
@@ -0,0 +1,9 @@
+import { Router } from "express";
+
+const router = Router();
+
+router.get('/', (request, response) => {
+    return response.json({ message: "Funcionou" });
+})
+
+export { router }
